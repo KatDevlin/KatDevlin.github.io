0b7a5a30a9f8a63600a37ebde30fef0077373e85
KatDev <kathleen.devlin@yale.edu>
2017-09-07 08:28:08 -0400

    update config

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/katdevlin/Desktop/blog/KatDevlin.github.io")
+
+#render your site. 
+rmarkdown::render_site()
