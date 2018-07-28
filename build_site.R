1ab0a49c77201726e9fc7de8a3cbb2ad9118207b
KatDev <kathleen.devlin@yale.edu>
2018-07-28 11:10:17 -0400

    formatting change

--- build_site.R
+++ build_site.R
@@ -1,8 +1,8 @@
-#Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
+# Set our working directory. 
+# This helps avoid confusion if our working directory is 
+# not our site because of other projects we were 
+# working on at the time. 
 setwd("/Users/katdevlin/Desktop/blog/KatDevlin.github.io")
 
-#render your site. 
+# render your site. 
 rmarkdown::render_site()
